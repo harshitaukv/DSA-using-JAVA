e82ff5483dd182a5c71baf39f98168d5cc204695
harshitaukv <146750492+harshitaukv@users.noreply.github.com>
2026-06-09 16:39:37 +0530

    Add files via upload

---
+++ Linked List/swapNodes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode first = head;
+        ListNode second = head;
+        ListNode temp = head;
+        for (int i = 1; i < k; i++) {
+            first = first.next;
+        }
+
+        temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
+            second = second.next;
+        }
+
+        int t = first.val;
+        first.val = second.val;
+        second.val = t;
+
+        return head;
+    }
+}
